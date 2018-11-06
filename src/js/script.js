3802f47e8ddc6ba4f56410079cbc17e063458613
aleksey leichenko <ol.leichenko@gmail.com>
2018-11-06 22:46:58 +0200

    html nav-dropdawn menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".carousel__list").slick({nextArrow:'<div class="arr-next">&gt</div>',prevArrow:'<div class="arr-prev">&lt</div>'})});
+console.log("a"),console.log("b"),console.log("a"),console.log("b"),console.log("a"),console.log("b"),console.log("a"),console.log("b"),console.log("a"),console.log("b"),console.log("a"),console.log("b"),console.log("a"),console.log("b"),$(document).ready(function(){$(".carousel__list").slick({nextArrow:'<div class="arr-next">&gt</div>',prevArrow:'<div class="arr-prev">&lt</div>'})});
